8accb0a1ed18d262e0d8171eddcf3a554fe3b7d5
Dawid Kmak <dkmak02@gmail.com>
2023-11-07 21:08:44 +0100

    fixed

--- Skrypty/start.py
+++ Skrypty/start.py
@@ -2,9 +2,9 @@ import subprocess
 python_script_command = "python createNewData.py"
 pytonhscript = subprocess.Popen(python_script_command, shell=True)
 pytonhscript.wait()
-compile_command = "javac ..\\TinyGP-Java-master\\tiny_gp.java"
+compile_command = "javac -d ..\\TinyGP-Java-master\\bin ..\\TinyGP-Java-master\\*.java"
 subprocess.run(compile_command, shell=True)
-java_command = "java -cp ..\\TinyGP-Java-master tiny_gp"
+java_command = "java -cp ..\\TinyGP-Java-master\\bin Starter"
 java_process = subprocess.Popen(java_command, shell=True)
 java_process.wait()
 python_script_command2 = "python createExcelFile.py"
--- TinyGP-Java-master/.idea/sonarlint/issuestore/index.pb
+++ TinyGP-Java-master/.idea/sonarlint/issuestore/index.pb
@@ -5,12 +5,8 @@
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 ;
 problem.dat,1\e\1e9f9e943f39e4132ae16a59faaa7a193ed9bf8c
-<
-tiny_gp.java,7\1\71f46e71db20e3748822b5182059c75a1116b5f9
 ;
 TinyGP.java,4\1\41659241e66efc3687cbe0715c7e82b59d0cc984
->
-Constatns.java,e\4\e437febcfa6508c3db0d5740f43278dbe8091414
 >
 Constants.java,6\3\6370735d72eea0922969bbee38e8dc795454a316
 <
